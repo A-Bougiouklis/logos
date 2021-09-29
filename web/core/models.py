0dacff7b39ac35f98cc03864235e53a0b5d94b28
andreasbougiouklis@gmail.com <andreasbougiouklis@gmail.com>
2021-09-29 20:07:49 +0300

    first groups and relationships

--- web/core/models.py
+++ web/core/models.py
@@ -1,17 +1,24 @@
-from neomodel import (config, StructuredNode, StringProperty, IntegerProperty,
-    UniqueIdProperty, RelationshipTo)
+from neomodel import (
+    config,
+    StructuredNode,
+    StringProperty,
+    IntegerProperty,
+    UniqueIdProperty,
+    RelationshipTo,
+    StructuredRel,
+)
+from neomodel.contrib import SemiStructuredNode
+
 
 config.DATABASE_URL = 'bolt://neo4j:password@localhost:7687'
 
+class GroupRel(StructuredRel):
+    something = StringProperty()
 
-class Country(StructuredNode):
-    code = StringProperty(unique_index=True, required=True)
+class Token(SemiStructuredNode):
+    lemma = StringProperty(unique_index=True, required=True)
 
-
-class Person(StructuredNode):
-    uid = UniqueIdProperty()
-    name = StringProperty(unique_index=True)
-    age = IntegerProperty(index=True, default=0)
-
-    # traverse outgoing IS_FROM relations, inflate to Country objects
-    country = RelationshipTo(Country, 'IS_FROM')
+    nsbuj = RelationshipTo('Token', 'NSUBJ', model=GroupRel)
+    aux = RelationshipTo('Token', 'AUX', model=GroupRel)
+    xcomp = RelationshipTo('Token', 'XCOMP', model=GroupRel)
+    acomp = RelationshipTo('Token', 'ACOMP', model=GroupRel)
--- web/core/views.py
+++ web/core/views.py
@@ -1,51 +1,23 @@
 # Create your views here.
-from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
-from neomodel import RelationshipTo, StructuredNode, StringProperty, RelationshipDefinition, EITHER
-from neomodel.contrib import SemiStructuredNode
 
 def home(request):
-    # book = Book(title="Harry", format="A", status="A").save()
-    # shelf = Shelf(name="cool").save()
-    # return HttpResponse(f"Hello")
-    return create_dynamically_nodes()
+    return create_coded_nodes()
 
 def create_coded_nodes():
-    andreas = Person(name="Andreas", age=1).save()
-    greece = Country(code="Germany").save()
-    andreas.country.connect(greece)
-    return HttpResponse(f"{len(andreas.nodes.all())} {andreas} {greece} {andreas.country.is_connected(greece)}")
+    for i in Token.nodes.all():
+        i.delete()
 
+    andreas = Token(lemma="Andreas").save()
+    start = Token(lemma="start").save()
+    did = Token(lemma="did").save()
+    becoming = Token(lemma="becoming").save()
+    popular = Token(lemma="popular").save()
 
+    start.nsbuj.connect(andreas, {'something': "Hello"})
+    start.aux.connect(did, {'something': "Hello You"})
+    start.xcomp.connect(becoming, {'something': "Hello You"})
+    becoming.acomp.connect(popular, {'something': "Hello You"})
 
-# Create two Structured Nodes
-country = type(
-    "Country_1",
-    (SemiStructuredNode,),
-    {"name": StringProperty()}
-)
-person = type(
-    "Person_1",
-    (SemiStructuredNode,),
-    {
-        "name": StringProperty(),
-        "is_from": RelationshipTo(country, 'IS_FROM')
-    }
-)
-
-def create_dynamically_nodes():
-
-    # Now we need to create an object of that class
-    nikola = person(name="Nikola Tesla").save()
-    greece = country(name="Greece").save()
-
-    # create property on the fly
-    greece.skata = "5"
-    greece.save()
-
-    europe = country(name="Europe").save()
-
-    nikola.is_from.connect(greece)
-
-    return HttpResponse(f"{len(nikola.nodes.all())} {nikola} {greece.skata} {nikola.is_from.is_connected(greece)}")
+    return HttpResponse(f"{len(andreas.nodes.all())} {andreas} {start} {andreas.nsbuj.is_connected(start)}")
