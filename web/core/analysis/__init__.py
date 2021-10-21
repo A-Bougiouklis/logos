15c060c96e5de057cc0ab062a9c3f9a524297be4
andreasbougiouklis@gmail.com <andreasbougiouklis@gmail.com>
2021-10-21 23:23:13 +0300

    update training to accomodate the new graph

--- web/core/analysis/__init__.py
+++ web/core/analysis/__init__.py
@@ -1,29 +1,44 @@
-from web.core.models import Token
 from spacy.tokens.doc import Doc as spacy_doc
 
-from web.core.analysis.chunking import chunking
-from web.core.analysis.token_graph import dependency_sentence_graph
+from web.core.models import Entity, EntitySet
+from .chunking import find_chunks
+from .entity_graph import generate_entity_graph
+from .phrase_identifier import group_tokens_to_phrases
 from web.core.analysis.nlp_models import nlp
 
 
 def document_analysis(document: str):
 
-    doc_id = Token.nodes.max_document_id + 1
-    sen_id = Token.nodes.max_sentence_id + 1
+    doc_id = Entity.nodes.max_document_id + 1
+    sen_id = Entity.nodes.max_sentence_id + 1
 
-    cached_nodes = {}
+    cached_entity_nodes, cached_entity_set_nodes = {}, {}
 
     doc = nlp(document)
     for sent in doc.sents:
         # We convert the sentence to a doc to remove any index connection between the
         # tokens and the original doc.
-        cached_nodes = sentence_analysis(sent.as_doc(), doc_id, sen_id, cached_nodes)
+        cached_entity_nodes, cached_entity_set_nodes = sentence_analysis(
+            sent.as_doc(),
+            doc_id,
+            sen_id,
+            cached_entity_nodes,
+            cached_entity_set_nodes,
+        )
         sen_id += 1
 
 
 def sentence_analysis(
-        sent: spacy_doc, doc_id: int, sent_id: int, cached_nodes: dict[str, Token]
-) -> dict[str, Token]:
-    cached_nodes = dependency_sentence_graph(sent, doc_id, sent_id, cached_nodes)
-    chunking(sent)
-    return cached_nodes
+        sent: spacy_doc,
+        doc_id: int,
+        sent_id: int,
+        cached_entity_nodes: dict[str, Entity],
+        cached_entity_set_nodes: dict[str, EntitySet],
+) -> type[dict[str, Entity], dict[str, EntitySet]]:
+
+    phrases = group_tokens_to_phrases(sent, find_chunks(sent))
+    cached_entity_nodes, cached_entity_set_nodes = generate_entity_graph(
+        phrases, doc_id, sent_id, cached_entity_nodes, cached_entity_set_nodes
+    )
+
+    return cached_entity_nodes, cached_entity_set_nodes
