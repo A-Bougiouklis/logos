f70c869a584d48e58c7dc8d8e1c9456d8d85c920
andreasbougiouklis@gmail.com <andreasbougiouklis@gmail.com>
2021-10-21 22:57:15 +0300

    new entity graph

---
+++ web/core/tests/analysis/test_entity_graph.py
@@ -0,0 +1,60 @@
+from django.test import TestCase
+from neomodel import clear_neo4j_database
+
+from web.core.models import db, Entity, EntitySet
+from web.core.analysis.nlp_models import nlp
+from web.core.analysis.entity_graph import generate_entity_graph
+from web.core.analysis.phrase_identifier import group_tokens_to_phrases, Phrase
+from web.core.analysis.chunking import find_chunks
+
+
+class GenerateEntityGraph(TestCase):
+
+    def setUp(self):
+        clear_neo4j_database(db)
+
+    def entity_graph_asserts(
+            self, phrases: list[Phrase], doc_id: int, sent_id: int
+    ):
+        for index, phrase in enumerate(phrases[:-1]):
+
+            if phrase.node_type == Entity:
+                entity_node = Entity.nodes.get(text=phrase.span.text)
+            else:
+                entity_node = EntitySet.nodes.get(text=phrase.span.text)
+
+            with self.subTest(f"creates entity -> {phrase.span.text}"):
+                self.assertEqual(phrase.span.text, entity_node.text)
+
+            try:
+                with self.subTest(
+                    f"({phrase.span.text})-[SENTENCE]-({phrases[index+1].span.text})"
+                ):
+                    next_entity_node = Entity.nodes.get(text=phrases[index+1].span.text)
+                    r = entity_node.sentence.relationship(next_entity_node)
+
+                    self.assertIsNotNone(r)
+                    self.assertEqual(doc_id, r.document_id)
+                    self.assertEqual(sent_id, r.sentence_id)
+                    self.assertEqual(index, r.order)
+            except IndexError:
+                pass
+
+    def test_dependency_sentence_graph_without_cache(self):
+
+        doc = nlp("the big dog ate some poop and then got away from the police.")
+        phrases = group_tokens_to_phrases(doc, find_chunks(doc))
+        generate_entity_graph(phrases, 1, 2, {}, {})
+        self.entity_graph_asserts(phrases, 1, 2)
+
+    def test_dependency_sentence_graph_with_cache(self):
+        doc = nlp("the big dog ate some poop and then got away from the police.")
+        phrases = group_tokens_to_phrases(doc, find_chunks(doc))
+        cached_entities, cached_entity_sets = generate_entity_graph(
+            phrases, 1, 2, {}, {}
+        )
+
+        doc = nlp("the big dog ate a cow.")
+        phrases = group_tokens_to_phrases(doc, find_chunks(doc))
+        generate_entity_graph(phrases, 2, 3, cached_entities, cached_entity_sets)
+        self.entity_graph_asserts(phrases, 2, 3)
