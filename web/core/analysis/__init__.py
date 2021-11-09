a38cf2e90d18e4e463d9041682aac9cf8ffe49a8
andreasbougiouklis@gmail.com <andreasbougiouklis@gmail.com>
2021-11-09 15:30:23 +0200

    small update

--- web/core/analysis/__init__.py
+++ web/core/analysis/__init__.py
@@ -13,17 +13,17 @@ def document_analysis(document: str, cached_nodes):
     doc_id = Entity.nodes.max_document_id + 1
     sen_id = Entity.nodes.max_sentence_id + 1
 
-    cached_entity_nodes = {}
+    # cached_entity_nodes = {}
 
     doc = nlp(document)
     for sent in doc.sents:
         # We convert the sentence to a doc to remove any index connection between the
         # tokens and the original doc.
-        cached_entity_nodes, cached_entity_set_nodes = sentence_analysis(
+        cached_nodes = sentence_analysis(
             sent.as_doc(),
             doc_id,
             sen_id,
-            cached_entity_nodes,
+            cached_nodes,
         )
         sen_id += 1
     return cached_nodes
@@ -32,14 +32,14 @@ def sentence_analysis(
         sent: spacy_doc,
         doc_id: int,
         sent_id: int,
-        cached_entity_nodes: dict[str, Entity],
+        cached_nodes: dict[str, Entity],
 ) -> type[dict[str, Entity], dict[str, EntitySet]]:
 
     phrases = group_tokens_to_phrases(sent, find_chunks(sent))
-    cached_entity_nodes, phrases = generate_entity_graph(
-        phrases, doc_id, sent_id, cached_entity_nodes
+    cached_nodes, phrases = generate_entity_graph(
+        phrases, doc_id, sent_id, cached_nodes
     )
-    phrases, cached_entity_nodes = property_setter(phrases, cached_entity_nodes)
-    update_rules(phrases, cached_entity_nodes)
+    phrases, cached_nodes = property_setter(phrases, cached_nodes)
+    update_rules(phrases, cached_nodes)
 
-    return cached_entity_nodes
+    return cached_nodes
